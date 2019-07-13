8bf22bc1e52be96aa0c4d961ae06925c59283a2e
unknown <apo.raffalli@gmail.com>
2019-07-13 21:40:00 +0200

    MAJ setup

--- checkHyperV.ps1
+++ checkHyperV.ps1
@@ -1,16 +1,10 @@
 ﻿#Requires -RunAsAdministrator
-$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-if ($testadmin -eq $false) {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-exit $LASTEXITCODE
-}
 $hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
 # Check if Hyper-V is enabled
 if($hyperv.State -eq "Enabled") {
     write-host "Hyper V est activé."
-    #exit 0
+    exit 0
 } else {
     write-host "Hyper V n'est pas activé."
-    #exit 1
+    exit 1
 }
