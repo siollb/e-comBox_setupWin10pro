0aa8f85f20f069e4252ceb7468356c1356b55cdb
unknown <apo.raffalli@gmail.com>
2019-07-29 17:37:29 +0200

    Ajout fichier identifiants d'accès

--- restartApplication.ps1
+++ restartApplication.ps1
@@ -6,4 +6,9 @@ docker rm -fv e-combox
 docker volume rm $(docker volume ls -qf dangling=true)
 docker pull aporaf/e-combox:1.0
 docker run -dit --name e-combox -v ecombox_data:/usr/local/apache2/htdocs/ --restart always -p 8888:80 aporaf/e-combox:1.0
+
+# Nettoyage des images
+docker rmi $(docker images -q -f dangling=true)
+
+# Lancement de l'application
 Start-Process "C:\Program Files\e-comBox\e-comBox.url"
