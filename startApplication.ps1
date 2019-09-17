604f376ed31c69e95436a3dbf5856d04ab8bea0a
unknown <apo.raffalli@gmail.com>
2019-09-17 23:30:31 +0200

    Ajout création du reseau des sites

--- startApplication.ps1
+++ startApplication.ps1
@@ -1,2 +1,11 @@
+# Création du réseau utilisé par les sites
+docker network create bridge_e-combox
+
+# Téléchargement d'e-comBox
+docker pull aporaf/e-combox:1.0
+
+# Lancement d'e-comBox
 docker run -dit --name e-combox -v ecombox_data:/usr/local/apache2/htdocs/ --restart always -p 8888:80 aporaf/e-combox:1.0
+
+# Lancement de l'URL
 Start-Process "C:\Program Files\e-comBox\e-comBox.url"
