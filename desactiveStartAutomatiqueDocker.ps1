5fa3790abdaf352ec9bdb3e67cbbf829ac42d914
unknown <apo.raffalli@gmail.com>
2019-11-27 00:31:40 +0100

    Mise à jour du logo

--- desactiveStartAutomatiqueDocker.ps1
+++ desactiveStartAutomatiqueDocker.ps1
@@ -2,3 +2,6 @@
 
 # Désactivation du démarrage automatique
 Set-Service -NAME com.docker.service -StartupType Manual
+
+# Suppression de Docker Desktop au démarrage
+Remove-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -name "Docker Desktop"
